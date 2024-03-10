c75f49ae98e9048e979ba5bff0891b0ee3a78020
Laura Stasiule <laura.stasiule@gmail.com>
2024-03-10 13:49:57 +0200

    Connecting observable and observer without operators, error and compete

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Observable } = require("rxjs");
+
+const observable = new Observable((subscriber) => {
+    //emit data
+    //convention subscriber - but can be any name
+    subscriber.next(10)
+})
+
+const observer = {
+    //ok
+    next: (value) => {
+        console.log("Observer got a value of " + value);
+    },
+    //error
+    error: (err) => {
+        console.log("Observer got an error of " + err);
+    },
+    // when observable wants to complete, no value passed
+    complete: () => {
+        console.log("Observer got a complete notification.");
+    }
+}
+
+// connecting observable and observer
+observable.subscribe(observer);
