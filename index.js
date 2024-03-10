3dcb4394e21b27c36d22bacbbe7f58fd565bbebf
Laura Stasiule <laura.stasiule@gmail.com>
2024-03-10 14:20:44 +0200

    Pipe and Operators

--- index.js
+++ index.js
@@ -1,12 +1,66 @@
-const { Observable } = require("rxjs");
+const { Observable, map } = require("rxjs");
+
+const users = {
+    data: [
+        {
+            status: "active",
+            age: 14
+        },
+        {
+            status: "inactive",
+            age: 32
+        },
+        {
+            status: "inactive",
+            age: 53
+        },
+        {
+            status: "active",
+            age: 17
+        },
+        {
+            status: "inactive",
+            age: 11
+        },
+        {
+            status: "inactive",
+            age: 32
+        },
+        {
+            status: "inactive",
+            age: 43
+        },
+        {
+            status: "active",
+            age: 23
+        },
+    ]
+}
 
 const observable = new Observable((subscriber) => {
     //emit data
     //convention subscriber - but can be any name
-    subscriber.next(10)
-    subscriber.next(11)
-    subscriber.next(12)
-})
+    subscriber.next(users)
+}).pipe(
+    // extracts data from an object
+    map((value) => {
+        console.log("1. Got data from observable", value);
+        return value.data;
+    }),
+    map((value) => {
+        console.log("2. Got data from from first operator", value);
+        return value.filter(user => user.status === 'active');
+    }),
+    map((value) => {
+        console.log("3. Got data from from second operator", value);
+        return (value.reduce((sum, user) => sum + user.age, 0)) / value.length;
+    }),
+    map((value) => {
+        console.log("4. Got data from from third operator", value);
+        if (value < 18) throw new Error('Average age is too young')
+        return value;
+    })
+)
 
 const observer = {
     //ok
